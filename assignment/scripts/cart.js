29159e1fa8b8c0bd88d6d6eaff7410c74ad3a37c
Jose Rubio <ru310z@gmail.com>
2021-05-28 18:01:54 -0500

    Functions addItem, listItems and empty added to file

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,19 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+let basket = [];
+
+function addItem( item ){
+  basket.push( item );
+  return true;
+}
+
+function listItems(){
+  for (item of basket){
+    console.log( item ) ;
+  }
+}
+
+function empty(){
+  basket = [];
+}

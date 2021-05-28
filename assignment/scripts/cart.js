2f3ff266ac45181cfbe6821f4889e3315c8376bf
Jose Rubio <ru310z@gmail.com>
2021-05-28 18:45:57 -0500

    Stretch goals 1, 2 and 3 added. More tests added

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -4,6 +4,10 @@ console.log('***** Cart Functions *****');
 let basket = [];
 
 function addItem( item ){
+  // Stretch goal
+  if ( isFull() ){
+    return false;
+  } //End of stretch goal
   basket.push( item );
   return true;
 }
@@ -17,12 +21,23 @@ function listItems(){
 function empty(){
   basket = [];
 }
+//Stretch goals
+const maxItems = 5;
 
+function isFull(){
+  //.length shows index so we add 1 to get total elements on basket
+  return (basket.length+1) > maxItems ? true : false;
+}
+//End of stretch goals
+
+//Testing code
 console.log('Adding apples (expect true)', addItem('apples'));
+console.log('Adding bananas (expect true)', addItem('bananas'));
+console.log('Adding watermelons (expect true)', addItem('watermelons'));
+console.log('Adding pineapples (expect true)', addItem('pineapples'));
+console.log('Adding mangos (expect true)', addItem('mangos'));
+console.log('Adding papaya (expect false)', addItem('papaya'));
 listItems();
-console.log('Adding apples (expect true)', addItem('bananas'));
-listItems();
-console.log('Adding apples (expect true)', addItem('watermelons'));
 console.log(`Before emptying basket is: ${basket}`);
 empty();
 console.log(`After emptying basket is: ${basket}`);

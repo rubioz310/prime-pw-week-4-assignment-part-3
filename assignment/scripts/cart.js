d62a4cf26288351b8cbbbe80234fc1af7274d7c9
Jose Rubio <ru310z@gmail.com>
2021-05-28 19:16:58 -0500

    Stretch goal 4 added. Final tests added

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -28,16 +28,28 @@ function isFull(){
   //.length shows index so we add 1 to get total elements on basket
   return (basket.length+1) > maxItems ? true : false;
 }
+
+function removeItem( item ){
+  let index = basket.indexOf( item );
+  if( index == -1 ){ return null } // If the item is not in the basket it will return -1
+  return basket.splice( index, 1 );
+}
 //End of stretch goals
 
 //Testing code
+//Adding items to basket
 console.log('Adding apples (expect true)', addItem('apples'));
 console.log('Adding bananas (expect true)', addItem('bananas'));
 console.log('Adding watermelons (expect true)', addItem('watermelons'));
 console.log('Adding pineapples (expect true)', addItem('pineapples'));
 console.log('Adding mangos (expect true)', addItem('mangos'));
-console.log('Adding papaya (expect false)', addItem('papaya'));
+console.log('Adding papaya (expect false)', addItem('papaya')); //Item number 6 not added
+//Listing all items
 listItems();
+//Removing item
+console.log(`Item removed: ${removeItem('watermelons')}`);
+console.log(`Item removed: ${removeItem('papaya')}`);
+//Emptying basket
 console.log(`Before emptying basket is: ${basket}`);
 empty();
 console.log(`After emptying basket is: ${basket}`);
